55c6783af63891f11a860048096df87348d491ba
djonzoumbek <guepelbekamis@gmail.com>
2024-11-01 01:48:55 +0000

    Ajout du fichier requirements.txt

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
